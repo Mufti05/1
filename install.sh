a7e2a5f2db71eb582e6c992cb06f5338022c4dbb
Mufti05 <59869552+Mufti05@users.noreply.github.com>
2020-01-25 19:32:57 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg update && pkg upgrade
+pkg install git
+pkg install bash
+pkg install nodejs
+pkg install php
+pkg install python2
+pkg install figlet
+pkg install sl
+pkg install cmatrix
+pip2 install requests
+pip2 install mechanize
